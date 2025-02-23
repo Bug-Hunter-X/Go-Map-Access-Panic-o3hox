6019a2aa1cc485b36181ad19770d16e5e7b080a2
BugHunter-X <askzenai@gmail.com>
2025-02-23 10:40:05 +0100

    Add bugSolution.go file

---
+++ bugSolution.go
@@ -0,0 +1,20 @@
+```go
+package main
+
+import (
+
+
+    "fmt"
+)
+
+func main() {
+    var m map[string]int
+    // Check if m is nil before accessing
+    if m == nil {
+        m = make(map[string]int)
+    }
+    m["key"] = 10
+    fmt.Println(m["key"])
+    fmt.Println(m)
+} 
+```
